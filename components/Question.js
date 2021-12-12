20943867308216aba08bbe8dc8556f84589e344c
Tudor73 <tudorminciunescu@gmail.com>
2021-12-13 00:45:16 +0200

    question form

--- components/Question.js
+++ components/Question.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { RadioButton } from "react-native-paper";
 
 function Question() {
+  const [checked, setChecked] = useState(0);
   return (
     <View>
       <View style={styles.container}>
@@ -11,15 +12,40 @@ function Question() {
       <View style={styles.answersContainer}>
         <View style={styles.answer}>
           <Text>PRima optiune</Text>
-          <RadioButton status="checked" value="" />
+          <RadioButton
+            status={checked === 1 ? "checked" : "unchecked"}
+            uncheckedColor="black"
+            onPress={() => {
+              setChecked(1);
+            }}
+          />
         </View>
         <View style={styles.answer}>
           <Text>A doua optiune</Text>
-          <RadioButton status="checked" value="" />
+          <RadioButton
+            status={checked === 2 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked(2);
+            }}
+          />
         </View>
         <View style={styles.answer}>
           <Text>A treia optiune</Text>
-          <RadioButton status="checked" value="" />
+          <RadioButton
+            status={checked === 3 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked(3);
+            }}
+          />
+        </View>
+        <View style={styles.answer}>
+          <Text>A treia optiune</Text>
+          <RadioButton
+            status={checked === 4 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked(4);
+            }}
+          />
         </View>
       </View>
     </View>
@@ -44,8 +70,11 @@ const styles = StyleSheet.create({
   },
   answer: {
     display: "flex",
+    height: "25%",
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginLeft: "2%",
   },
 });
 
