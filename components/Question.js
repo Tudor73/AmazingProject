256964b298aa3e9c49b465e682a8e206290b34e7
AlexBlackLivesMatter <alexmihai200125@yahoo.com>
2021-12-13 00:45:28 +0200

    CheckBox

--- components/Question.js
+++ components/Question.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { RadioButton } from "react-native-paper";
 
 function Question() {
+  const [checked, setChecked] = useState(0);
   return (
     <View>
       <View style={styles.container}>
         <Text> Sa va dau muie cu drag!</Text>
       </View>
       <View style={styles.answersContainer}>
-        <View styles={styles.answer}>
+        <View style={styles.answer}>
           <Text>Multumesc!</Text>
-          <RadioButton status="checked" />
+          <RadioButton
+            status={checked === 1 ? "checked" : "unchecked"}
+            uncheckedColor="black"
+            onPress={() => {
+              setChecked(1);
+            }}
+          />
         </View>
-        <View styles={styles.answer}>
+        <View style={styles.answer}>
           <Text>Sa mil iei !</Text>
-          <RadioButton status="checked" />
+          <RadioButton
+            status={checked === 2 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked(2);
+            }}
+          />
         </View>
-        <View styles={styles.answer}>
+        <View style={styles.answer}>
           <Text>Buna treaba!</Text>
-          <RadioButton status="checked" />
+          <RadioButton
+            status={checked === 3 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked(3);
+            }}
+          />
         </View>
-        <View styles={styles.answer}>
+        <View style={styles.answer}>
           <Text>Sa tragi de el!</Text>
-          <RadioButton status="checked" />
+          <RadioButton
+            status={checked === 4 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked(4);
+            }}
+          />
         </View>
       </View>
     </View>
@@ -37,6 +59,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "black",
     marginLeft: "10%",
+    marginBottom: "2%",
   },
   answersContainer: {
     width: "80%",
@@ -47,8 +70,10 @@ const styles = StyleSheet.create({
   },
   answer: {
     display: "flex",
+    height: "25%",
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
   },
 });
 
