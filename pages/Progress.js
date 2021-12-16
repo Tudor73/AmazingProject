110a760bfe6eedc88a6e665a1d6c298bc95f9e59
Tudor73 <tudorminciunescu@gmail.com>
2021-12-16 23:58:14 +0200

    whatever

--- pages/Progress.js
+++ pages/Progress.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import Quiz from "../components/Quiz";
 
 function Progress() {
   return (
     <View>
       <Text>Progress</Text>
-      <Quiz />
     </View>
   );
 }
